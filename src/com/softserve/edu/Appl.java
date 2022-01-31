889d97d1e2dc105b885efe312caeeb39af080b0c
bebebe <hahaha@gmail.com>
2022-01-31 19:57:21 +0200

    Add Hello World

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -15,7 +15,7 @@ public class Appl {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello W");
+		System.out.println("Hello World");
 		//
 		int i = 456;
 		System.out.println("main i= " + i);
