91629ed25073a80322cc2b0fe5f33da51ce55385
vsvldbrnsh <brnshv@gmail.com>
2022-01-31 22:19:32 +0200

    deleted edu/Appl.java , hm1/Solution.java. added edu/hm1/ edu/lessons/

--- src/com/softserve/edu/hm1/Solution.java
+++ src/com/softserve/edu/hm1/Solution.java
@@ -1,7 +1,7 @@
-package com.softserve.hm1;
+package com.softserve.edu.hm1;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hi");
     }
 }
