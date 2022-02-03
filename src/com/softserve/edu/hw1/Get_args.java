e0116eafd5c07afe1ceaf0cbdaea1e55c4cf93ef
Taras_Kaminskyi <tkaminskyi@gmail.com>
2022-02-03 17:31:48 +0200

    Get some args

---
+++ src/com/softserve/edu/hw1/Get_args.java
@@ -0,0 +1,9 @@
+package com.softserve.edu.hw1;
+
+public class Get_args {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
