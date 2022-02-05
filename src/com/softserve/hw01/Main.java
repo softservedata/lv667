1780ab38d1008d739fe1dbe252f278ef202c8f83
Ivan Kopytko <kopytkoiv@gmail.com>
2022-02-05 14:10:27 +0200

    Done homework task #2

--- src/com/softserve/hw01/Main.java
+++ src/com/softserve/hw01/Main.java
@@ -2,7 +2,11 @@ package com.softserve.hw01;
 
 public class Main {
     public static void main(String[] args) {
-        FlowerBed flowerBed = new FlowerBed();
-        flowerBed.calculateAreaAndPerimeter();
+//        FlowerBed flowerBed = new FlowerBed();
+//        flowerBed.calculateAreaAndPerimeter();
+
+        NameAndAddress nameAndAddress = new NameAndAddress();
+        nameAndAddress.findOutNameAndAddress();
+        //new NameAndAddress().findOutNameAndAddress();
     }
 }
