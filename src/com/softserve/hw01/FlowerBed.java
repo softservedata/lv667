84d8f6d72df1f2afdf2997b44ce1c89e4db72790
Ivan Kopytko <kopytkoiv@gmail.com>
2022-02-07 14:22:37 +0200

    Fix HM Task 1.2.3

--- src/com/softserve/hw01/FlowerBed.java
+++ src/com/softserve/hw01/FlowerBed.java
@@ -1,6 +1,6 @@
 package com.softserve.hw01;
 
-import org.apache.commons.math3.util.Precision;
+//import org.apache.commons.math3.util.Precision;
 
 import java.util.Scanner;
 
@@ -14,13 +14,17 @@ public class FlowerBed {
         System.out.println("Write the radius");
         r = Integer.parseInt(sc.nextLine());
         System.out.println("Radius: " + r);
-        //perimeter = 2 * pi * r;
-        //square = pi * Math.pow(r, 2);
+//        perimeter = 2 * pi * r;
+//        square = pi * Math.pow(r, 2);
         //System.out.println("Perimeter = " + Precision.round(perimeter, 2));
-        System.out.println("Perimeter = " + Precision.round(calculatePerimeter(r), 2));
+//        System.out.println("Perimeter = " + Precision.round(calculatePerimeter(r), 2));
         //System.out.print("Square = " + Precision.round(square, 2));
-        System.out.print("Square = " + Precision.round(calculateArea(r), 2));
-        //System.out.format("%.2f",p);   // alternative round
+//        System.out.print("Square = " + Precision.round(calculateArea(r), 2)); // alternative round
+        System.out.print("Perimeter = ");
+        System.out.format("%.2f",calculatePerimeter(r));
+        System.out.print("\r\nArea = ");
+        System.out.format("%.2f",calculateArea(r));
+
         sc.close();
     }
 
--- src/com/softserve/hw01/Main.java
+++ src/com/softserve/hw01/Main.java
@@ -2,15 +2,15 @@ package com.softserve.hw01;
 
 public class Main {
     public static void main(String[] args) {
-//        FlowerBed flowerBed = new FlowerBed();
-//        flowerBed.calculateAreaAndPerimeter();
+        FlowerBed flowerBed = new FlowerBed();
+        flowerBed.calculateAreaAndPerimeter();
 //        // new FlowerBed().calculateAreaAndPerimeter();
 
 //         NameAndAddress nameAndAddress = new NameAndAddress();
 //         nameAndAddress.findOutNameAndAddress();
-//         // new NameAndAddress().findOutNameAndAddress();
+         // new NameAndAddress().findOutNameAndAddress();
 
-        PhoneCalls phoneCalls = new PhoneCalls();
-        phoneCalls.costOfCalls();
+//        PhoneCalls phoneCalls = new PhoneCalls();
+//        phoneCalls.costOfCalls();
     }
 }
