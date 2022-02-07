caa3ab1a3f56698e980c296f07a098f6a86d16c7
Chelovechec <mukolastarosilski@gmail.com>
2022-02-07 13:13:34 +0200

    Homework_04

---
+++ src/com/softserve/edu_04/H4r.java
@@ -0,0 +1,17 @@
+package com.softserve.edu_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class H4r {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Hi. What is your name?");
+        String name = br.readLine();
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(br.readLine());
+        System.out.println("Hello " + name);
+        System.out.println("Your are " + age);
+    }
+}
