c1f4ff0d729597252b2ec2a499744b9e3bc4f45e
lanaS01 <svitlana.sadvarii@gmail.com>
2022-02-07 14:27:34 +0200

    add hw04

--- src/com/softserve/hw04/Bad.java
+++ src/com/softserve/hw04/Bad.java
@@ -1,2 +1,25 @@
-package com.softserve.hw04;public class Bad {
+package com.softserve.hw04;
+
+import java.util.Scanner;
+
+public class Bad {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("The radius is: ");
+        double radius = Double.parseDouble (sc.nextLine());
+        double perimeter = (2 * 3.141592653589793 * radius);
+        System.out.println("Perimeter of circle is: " + perimeter);
+        double area = (3.141592653589793 * radius * radius);
+        System.out.println("Area of circle is: " + area);
+        System.out.println();
+        try {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("The answer is: " + perimeter +", " + area + ". ");
+        sc.close();
+    }
+
 }
