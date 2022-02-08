5466f5d637be6389ecd791150bc07e4949ee3370
Vasyl <mr.shtoiko@gmail.com>
2022-02-08 19:16:22 +0200

    Add home work #5
    - Add Shtoiko/src/com/softserve/homework05/Person.java
    - Add Shtoiko/src/com/softserve/homework05/Main.java

--- Shtoiko/src/com/softserve/homework05/Main.java
+++ Shtoiko/src/com/softserve/homework05/Main.java
@@ -1,2 +1,16 @@
-package com.softserve.homework05.practical;public class zxc {
+package com.softserve.homework05;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person first = new Person();
+        first.input();
+        System.out.println(first.output());
+        Person second = new Person("Igor", "Shevchenko", 1990);
+        System.out.println(second.output());
+        Person third = new Person("Andryi", "Terasenko");
+        Person fourth = new Person("Oleg", "Onykienko");
+        System.out.println(fourth.output());
+        Person fifth = new Person("Taras", "Uhimenko", 1995);
+    }
 }
--- Shtoiko/src/com/softserve/homework05/Person.java
+++ Shtoiko/src/com/softserve/homework05/Person.java
@@ -1,2 +1,53 @@
-package com.softserve.homework05;public class Person {
+package com.softserve.homework05;
+
+import java.util.Scanner;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+   public Person(){}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public int getAge(){
+       return 2022 - birthYear;
+    }
+
+    public void input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name");
+        firstName = scanner.nextLine();
+        System.out.println("Enter last name");
+        lastName = scanner.nextLine();
+        System.out.println("Enter birth year");
+        birthYear = scanner.nextInt();
+    }
+
+    public String output(){
+       String message = "";
+       if (firstName != null){
+           message = "First name: " + firstName + "\nLast Name: " + lastName;
+       }
+       if (birthYear != 0){
+           message += "\nBirth year: " +
+                   birthYear + "\nAge: " + this.getAge();
+       }
+       return message;
+    }
+
+    public void changeName(String firstName, String lastName){
+       this.firstName = firstName;
+       this.lastName = lastName;
+    }
 }
