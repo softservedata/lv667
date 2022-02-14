4f3958b9a80b9965324004122170b112d83732da
Taras_Kaminskyi <tkaminskyi@gmail.com>
2022-02-14 17:39:39 +0200

    Home work 6.1.2 4. Condition statements

---
+++ src/com/softserve/edu/hw6/Main_find_max_min.java
@@ -0,0 +1,52 @@
+package com.softserve.edu.hw6;
+
+import java.util.Scanner;
+
+public class Main_find_max_min {
+    private int i1, i2, i3, max, min;
+
+    private Main_find_max_min() {
+
+    }
+
+    private Main_find_max_min(int i1, int i2, int i3) {
+        this.i1 = i1;
+        this.i2 = i2;
+        this.i3 = i3;
+        this.max = i1;
+        this.min = i1;
+    }
+
+    public static void main(String[] args) {
+        Main_find_max_min objMaxMin = new Main_find_max_min(inputFloat("Input i1:"), inputFloat("Input i2:"), inputFloat("Input i3:"));
+        objMaxMin.findMaxMin(objMaxMin.i1);
+        objMaxMin.findMaxMin(objMaxMin.i2);
+        objMaxMin.findMaxMin(objMaxMin.i3);
+        objMaxMin.printOut();
+    }
+
+    public static int inputFloat(String txt) {
+        Scanner floatValue = new Scanner(System.in);
+        System.out.println(txt);
+        int inputI = floatValue.nextInt();
+        return inputI;
+    }
+
+    public void findMaxMin(int number) {
+        if(number > this.max) {
+            this.max = number;
+        }
+        if(number < this.min) {
+            this.min = number;
+        }
+
+    }
+
+
+    public void printOut() {
+        System.out.println(this.i1 + " " + this.i2 + " " + this.i3);
+        System.out.println("max " + this.max);
+        System.out.println("min " + this.min);
+    }
+
+}
