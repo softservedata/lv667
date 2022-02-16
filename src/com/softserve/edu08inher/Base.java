22a51e3723a332f2e274a68734bb2836f3afe6ca
bebebe <hahaha12@gmail.com>
2022-02-16 20:55:25 +0200

    OOP. Inheritance

---
+++ src/com/softserve/edu08inher/Base.java
@@ -0,0 +1,10 @@
+package com.softserve.edu08inher;
+
+public abstract class Base {
+
+    public Base() {
+        System.out.println("constructor Base()");
+    }
+
+    public abstract int getI();
+}
