bdea26adb908eb69d1c047c322d9ba329d2ea47f
Ivan Kopytko <kopytkoiv@gmail.com>
2022-02-17 15:42:57 +0200

    Lesson 5. Condition statements Homework Fix

--- src/com/softserve/hw04/PT/Employee.java
+++ src/com/softserve/hw04/PT/Employee.java
@@ -13,51 +13,24 @@ public class Employee {
         this.salary = salary;
     }
 
-    public void display() {
-        System.out.println("Name: " + name);
-        System.out.println("Department number: " + departmentNumber);
-        System.out.println("Salary: " + salary);
+    public int getDepartmentNumber() {
+        return departmentNumber;
     }
 
+    public int getSalary() {
+        return salary;
+    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Employee employeeArray[] = new Employee[5];
-        employeeArray[0] = new Employee("Emma Watson", 20, 5200);
-        employeeArray[1] = new Employee("Mary Clinton", 5, 4500);
-        employeeArray[2] = new Employee("Tom Washington", 20, 6800);
-        employeeArray[3] = new Employee("Victor Spielberg", 5, 4200);
-        employeeArray[4] = new Employee("Clark Wills", 5, 5000);
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
-        System.out.print("Enter department number: ");
-        int dpNumber = sc.nextInt();
-        int count = 0;
-        for (int i = 0; i < employeeArray.length; i++) {
-            if (dpNumber == employeeArray[i].departmentNumber) {
-                System.out.println("");
-                count++;
-                employeeArray[i].display();
-            }
-        }
-        if (count == 0) {
-            System.out.println("There is no employee in department #" + dpNumber);
-        }
-        int tmp;
-        for (int i = 0; i < employeeArray.length - 1; i++) {
-            for (int j = i + 1; j < employeeArray.length; j++) {
-                if (employeeArray[i].salary < employeeArray[j].salary) {
-                    tmp = employeeArray[i].salary;
-                    employeeArray[i].salary = employeeArray[j].salary;
-                    employeeArray[j].salary = tmp;
-                }
-            }
-        }
-        System.out.println("\r\nWorkers by the field salary in descending order:");
-        for (int i = 0; i < employeeArray.length; i++) {
-            employeeArray[i].display();
-        }
+    public void display() {
+        System.out.println("Name: " + name+", department number: " + departmentNumber+", Salary: " + salary);
     }
 
 }
+
+
 
 
---
+++ src/com/softserve/hw04/PT/MainEmployee.java
@@ -0,0 +1,45 @@
+package com.softserve.hw04.PT;
+
+import java.util.Scanner;
+
+public class MainEmployee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Employee employeeArray[] = new Employee[5];
+        employeeArray[0] = new Employee("Emma Watson", 20, 5200);
+        employeeArray[1] = new Employee("Mary Clinton", 5, 4500);
+        employeeArray[2] = new Employee("Tom Washington", 20, 6800);
+        employeeArray[3] = new Employee("Victor Spielberg", 5, 4200);
+        employeeArray[4] = new Employee("Clark Wills", 5, 5000);
+
+        System.out.print("Enter department number: ");
+        int dpNumber = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < employeeArray.length; i++) {
+            if (dpNumber == employeeArray[i].getDepartmentNumber()) {
+                count++;
+                employeeArray[i].display();
+            }
+        }
+        if (count == 0) {
+            System.out.println("There is no employee in department #" + dpNumber);
+        }
+        int tmp;
+        for (int i = 0; i < employeeArray.length - 1; i++) {
+            for (int j = i + 1; j < employeeArray.length; j++) {
+                if (employeeArray[i].getSalary() < employeeArray[j].getSalary()) {
+                    tmp = employeeArray[i].getSalary();
+                    employeeArray[i].setSalary(employeeArray[j].getSalary());
+                    employeeArray[j].setSalary(tmp);
+                }
+            }
+        }
+        System.out.println("\r\nWorkers by the field salary in descending order:");
+        for (int i = 0; i < employeeArray.length; i++) {
+            employeeArray[i].display();
+        }
+    }
+
+}
+
+
