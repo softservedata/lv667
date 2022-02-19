989678e6f422144c0c1b55a49cd4d5336011f231
TarasBalaban <taras.skif@gmail.com>
2022-02-20 01:16:00 +0200

    Add TarasBalaban version

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -15,7 +15,7 @@ public class Appl {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
 		//
 		int i = 456;
 		System.out.println("main i= " + i);
