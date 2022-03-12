6a2f9c234b03cc8aa5880d89bc158a2a453d9267
Jaroslav <garasym@gmail.com>
2022-03-12 14:10:26 +0200

    Update Class

--- src/com/softserve/edu01/edu/Appl.java
+++ src/com/softserve/edu01/edu/Appl.java
@@ -18,7 +18,7 @@ public class Appl {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!!");
 		System.out.println("Dev");
 		//
 		int i = 456;
