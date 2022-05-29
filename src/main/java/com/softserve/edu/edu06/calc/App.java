c885f65b159fc794433555a10f0c53fea0e09f55
vsvldbrnsh <brnshv@gmail.com>
2022-05-29 13:24:37 +0300

    add tests

---
+++ src/main/java/com/softserve/edu/edu06/calc/App.java
@@ -0,0 +1,11 @@
+package com.softserve.edu.edu06.calc;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        System.out.println("4 + 4 = " + calc.add(4, 4));
+        System.out.println("20 / 4 = " + calc.div(20, 4));
+    }
+}
